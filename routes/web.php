610aa4f67f099acfc8cf9053c8b9db96fdf3115c
luqmanmuiz <luqmanmuizz03@gmail.com>
2023-08-20 14:28:23 +0800

    first commit

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\IndexController;
+use App\Http\Controllers\ListingController;
+
+/*
+@@ -11,4 +12,7 @@
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [IndexController::class, 'index']);
+Route::get('/hello', [IndexController::class, 'show']);
+
+Route::resource('listing', ListingController::class);
