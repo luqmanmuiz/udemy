f298e4511a1d809939f52760a9ba1aa9f2178683
luqmanmuiz <luqmanmuizz03@gmail.com>
2023-08-24 22:11:13 +0800

    Updated README file

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,4 @@ use App\Http\Controllers\ListingController;
 Route::get('/', [IndexController::class, 'index']);
 Route::get('/hello', [IndexController::class, 'show']);
 
-Route::resource('listing', ListingController::class);
+Route::resource('/listing', ListingController::class);
